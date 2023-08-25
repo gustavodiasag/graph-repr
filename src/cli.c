51d3a20a04905238574c9053846a3736b3d9a26f
gustavodiasag <gustavodias.aguiar1@gmail.com>
2023-08-25 18:26:21 -0300

    style: project license

--- src/cli.c
+++ src/cli.c
@@ -1,3 +1,25 @@
+/**
+ * Copyright (c) 2023 Gustavo Dias de Aguiar
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include "cli.h"
 #include "common.h"
 
@@ -7,8 +29,8 @@
 
 /// @brief Optional structures supported.
 static const char* default_paths[] = {
-    ['l'] = "large-sample.txt",
-    ['s'] = "small-sample.txt",
+    ['l'] = "../large-sample.txt",
+    ['s'] = "../small-sample.txt",
 };
 
 /// @brief Displays error message to `stderr`.
@@ -53,7 +75,7 @@ const char* parse_args(int argc, char** argv)
     case '?':
         // Unsupported argument errors are directly reported by `getop`.
         help();
-        break;
+        return NULL;
     case 'l':
     case 's':
         return default_paths[opt];
