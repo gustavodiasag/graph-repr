9fe070739bc84056aef3acc4e1adeb05239ced78
gustavodiasag <gustavodias.aguiar1@gmail.com>
2023-09-28 19:29:35 -0300

    style: rename

---
+++ test/test_cli.cc
@@ -0,0 +1,37 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+    #include "../include/cli.h"
+}
+
+TEST(CliTest, NoArguments)
+{
+    const char* path = parse_args(1, NULL);
+
+    ASSERT_STREQ(path, NULL);
+}
+
+TEST(CliTest, SmallSampleFile)
+{
+    char* argv[] = {"foo", "-s"};
+
+    const char *path = parse_args(2, argv);
+
+    ASSERT_STREQ(path, "../small-sample.txt");
+}
+
+TEST(CliTest, LargeSampleFile)
+{
+    char* argv[] = {"foo", "-l"};
+
+    const char* path = parse_args(2, argv);
+    
+    ASSERT_STREQ(path, "../large-sample.txt");
+}
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
