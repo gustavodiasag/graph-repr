ebbbb4d7740ebf005df7af4aad65c0e74379c8f6
gustavodiasag <gustavodias.aguiar1@gmail.com>
2023-09-28 19:33:20 -0300

    initial graph representation

--- include/graph.h
+++ include/graph.h
@@ -1,40 +1,24 @@
-/**
- * Copyright (c) 2023 Gustavo Dias de Aguiar
-
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 #pragma once
 
-#include "common.h"
+typedef struct {
+    uint32_t* ptrs;
+    uint32_t* dest;
 
-typedef struct _edge {
-    uint32_t tail;
-    uint32_t head;
-} edge_t;
+    uint32_t _ptr_count;
+    uint32_t _dest_count;
+} Graph;
 
-typedef struct _graph {
-    uint32_t *ptrs;
-    uint32_t ptr_count;
-    edge_t *edges;
-    uint32_t edge_count;
-} graph_t;
+/// @brief Instantiates a directed graph given
+/// a set of vertices and edges.
+/// @param graph pointer to a graph object.
+/// @param fp file with the graph template.
+void build_graph(Graph* graph, FILE* fp);
 
-void init_graph(graph_t* graph);
-void build_graph(graph_t* graph, FILE *fp);
+/// @brief Deallocates memory from a graph.
+/// @param graph pointer to a graph object.
+void free_graph(Graph* graph);
+
+/// @brief Prints out the edges of the given
+/// graph to stdout.
+/// @param graph pointer to a graph object.
+void print_graph(Graph* graph);
