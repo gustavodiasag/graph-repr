9db0520eb8e7f3baa8a9b0e3910bda0efa9ca852
gustavodiasag <gustavodias.aguiar1@gmail.com>
2023-10-28 18:12:46 -0300

    feat: build graph from edge list

--- src/main.c
+++ src/main.c
@@ -2,14 +2,52 @@
 
 #include "cli.h"
 #include "common.h"
-#include "list.h"
+#include "graph.h"
+
+typedef struct _Vertex {
+    int     data;
+    int     weight;
+} Vertex;
+
+int vertex_match(const void* v1, const void* v2)
+{
+    return ((Vertex*)v1)->data - ((Vertex*)v2)->data;
+}
+
+void vertex_print(const void* data)
+{
+    const Vertex v = *(Vertex*)data;
+
+    printf("%d", v.data);
+}
 
 int main(int argc, char** argv)
 {
-    const char* path = parse_args(argc, argv);
-
-    List l = {0};
-    list_init(&l, sizeof(int), NULL);
-
+    if (argc != 2) {
+        err_abort("Input file was not provided.");
+    }
     
+    FILE *f = fopen(argv[1], "r");
+    if (!f) {
+        err_abort("Unable to open file.");
+    }
+    Graph g = {0};
+    graph_init(&g, sizeof(Vertex), vertex_match, NULL);
+
+    size_t vertices, edges;
+    fscanf(f, "%zu %zu", &vertices, &edges);
+
+    Vertex v, w;
+    while (fscanf(f, "%d %d", &v.data, &w.data) != EOF) {
+        printf("v: %d, %d\n", graph_ins_v(&g, &v), v.data);
+        printf("w: %d, %d\n", graph_ins_v(&g, &w), w.data);
+        graph_ins_e(&g, &v, &w);
+    }
+    fclose(f);
+
+    graph_print(&g, vertex_print);
+
+    graph_destroy(&g);
+
+    return 0;
 }
