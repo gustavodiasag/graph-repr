b42c4ad6eb07f06283fbbbb4591637f0f6e5e596
gustavodiasag <gustavodias.aguiar1@gmail.com>
2023-11-03 18:31:34 -0300

    feat(utils): extend stack library

--- src/utils/stack.c
+++ src/utils/stack.c
@@ -9,3 +9,13 @@ bool stack_pop(Stack* s, void** data)
 {
     return list_rm_next(s, NULL, data);
 }
+
+bool stack_contains(const Stack* s, const void* data)
+{
+    for (ListElt* e = list_head(s); e; e = list_next(e)) {
+        if (memcmp(list_data(e), data, list_dsize(s)) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
