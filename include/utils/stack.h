6260d8ddc8ebb6027f5e3c6f7d70d970f70be129
gustavodiasag <gustavodias.aguiar1@gmail.com>
2023-11-03 18:29:59 -0300

    feat(utils): extend stack library

--- include/utils/stack.h
+++ include/utils/stack.h
@@ -13,7 +13,10 @@ typedef List Stack;
  * Evaluates the data stored in the element at the top of a stack specified by
  * `s`.
  */
-#define stack_peek(s) ((s)->head ? (s)->head->data : NULL)
+#define stack_peek(s) ((s)->_head ? (s)->_head->_data : NULL)
+
+/** FIXME: Describe macro. */
+#define stack_new list_new
 
 /** 
  * Initializes a stack. Since a stack is based on a linked list and requires
@@ -26,6 +29,12 @@ typedef List Stack;
  * in the same way, `stack_destroy` is defined to `list_destroy`.
 */
 #define stack_destroy list_destroy
+
+/** FIXME: Describe function. */
+#define stack_is_empty list_is_empty
+
+/** FIXME: Describe function. */
+#define stack_print list_print
 
 /**
  * Pushes an element onto the top of a stack specified by `s`.
@@ -41,5 +50,8 @@ bool stack_push(Stack* s, const void* data);
  * Returns true if popping the element was successfull, or false otherwise.
  */
 bool stack_pop(Stack* s, void** data);
+
+/** FIXME: Describe function. */
+bool stack_contains(const Stack* s, const void* data);
 
 #endif
