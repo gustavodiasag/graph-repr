42f1ac63e7ccb5c4cfeab62a141ce62e69df2fcc
gustavodiasag <gustavodias.aguiar1@gmail.com>
2023-11-02 21:21:05 -0300

    feat: provide comparison for weighted vertices

--- include/vertex.h
+++ include/vertex.h
@@ -3,6 +3,8 @@
 
 #include <stdlib.h>
 
+#include "common.h"
+
 typedef struct _Vertex
 {
     int         _data;
@@ -14,6 +16,8 @@ typedef struct _Vertex
 #define vertex_weight(v) ((v)->_weight)
 
 int vertex_match(const void* v1, const void* v2);
+
+int vertex_compare(const void* v1, const void* v2);
 
 void vertex_print(const void* v);
 
--- src/vertex.c
+++ src/vertex.c
@@ -5,6 +5,16 @@ int vertex_match(const void* v1, const void* v2)
     return vertex_data((Vertex*)v1) - vertex_data((Vertex*)v2);
 }
 
+int vertex_compare(const void* v1, const void* v2)
+{
+#ifdef DEBUG_LOG
+    printf("vertex_compare: v1(%d, %zu), v2(%d, %zu).\n",
+        vertex_data((Vertex*)v1), vertex_weight((Vertex*)v1),
+        vertex_data((Vertex*)v2), vertex_weight((Vertex*)v2));
+#endif
+    return vertex_weight((Vertex*)v1) - vertex_weight((Vertex*)v2);
+}
+
 void vertex_print(const void* v)
 {
     printf("v: %d, w: %zu", vertex_data((Vertex*)v), vertex_weight((Vertex*)v));
