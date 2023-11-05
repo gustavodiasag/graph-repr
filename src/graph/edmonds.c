13812d6ed8e543f02619d6a08ea2755dfcea57c2
gustavodiasag <gustavodias.aguiar1@gmail.com>
2023-11-05 20:14:19 -0300

    feat(edmonds): cycle expansion

--- src/graph/edmonds.c
+++ src/graph/edmonds.c
@@ -4,6 +4,8 @@
 #include "utils/stack.h"
 #include "utils/vertex.h"
 
+#define VERTEX_RANGE(g) ((graph_v(g) + 1))
+
 /** FIXME: Describe function. */
 static void _edmonds(const Graph* g, const Graph* contracted, Graph* a);
 /** FIXME: Describe function. */
@@ -20,9 +22,20 @@ static void _contract_cycle(const Graph*    g,
                             Stack*          cycle,
                             Vertex**        parents);
 /** FIXME: Describe function. */
+static void _expand_cycle(const Graph*  g,
+                          Graph*        a,
+                          Stack*        cycle,
+                          Vertex**      parents);
+/** FIXME: Describe function. */
+static bool search_edge(const Graph* g, const void* v, const void* w);
+/** 
+ * Helper function to compare two `size_t` integers specified by `i` and `j`.
+ * 
+ * Returns a negative number 
+ */
 static int int_match(const void* i, const void* j);
 
-static void int_print(const void *i);
+static void int_print(const void* i);
 
 void edmonds(const Graph* g, Graph* a)
 {
@@ -30,8 +43,8 @@ void edmonds(const Graph* g, Graph* a)
 }
 
 static void _edmonds(const Graph* g, const Graph* contraction, Graph* a)
-{   
-    Vertex* parents[graph_v(g) + 1];
+{
+    Vertex* parents[VERTEX_RANGE(g)];
     memset(parents, NULL, sizeof(parents));
 
     _find_min_parents(contraction, parents);
@@ -50,7 +63,7 @@ static void _edmonds(const Graph* g, const Graph* contraction, Graph* a)
         _contract_cycle(contraction, &contracted, &cycle, parents);
         _edmonds(g, &contracted, a);
     }
-    // expand_cycle(g, a, &cycle, parents);
+    _expand_cycle(g, a, &cycle, parents);
 }
 
 static void _find_min_parents(const Graph* g, Vertex** parents)
@@ -117,7 +130,7 @@ static void _contract_cycle(const Graph*    g,
                             Stack*          cycle,
                             Vertex**        parents)
 {
-    size_t contracted_vertex = *(size_t*)(stack_peek(cycle));
+    size_t contracted_vertex = *(size_t*)stack_peek(cycle);
 
     for (ListElt* v = list_head(&g->_adjlists); v; v = list_next(v)) {
         const AdjList* src = (AdjList*)list_data(v);
@@ -153,6 +166,80 @@ static void _contract_cycle(const Graph*    g,
             }
         }
     }
+}
+
+static void _expand_cycle(const Graph*  g,
+                          Graph*        a,
+                          Stack*        cycle,
+                          Vertex**      parents)
+{
+    size_t* contracted_vertex = (size_t*)stack_peek(cycle);
+    Vertex* cycle_breakpoint;
+
+    bool visited[VERTEX_RANGE(g)];
+    memset(visited, false, sizeof(visited));
+
+    if (contracted_vertex) {
+        cycle_breakpoint = parents[*contracted_vertex];
+    }
+
+    size_t src, dest;
+    for (size_t i = 1; i < VERTEX_RANGE(g); i++) {
+        dest = i;
+        src = parents[dest] ? vertex_data(parents[dest]) : 0;
+        
+        while (src && !visited[dest]) {
+            Vertex exp_src = {
+                ._data = src,
+                ._weight = parents[src] ? vertex_weight(parents[src]) : 0
+            };
+            Vertex exp_dest = {
+                ._data = dest,
+                ._weight = parents[dest] ? vertex_weight(parents[dest]) : 0
+            };
+            if (search_edge(g, &exp_src, &exp_dest)) {
+                if (contracted_vertex) {
+                    if (int_match(&vertex_data(cycle_breakpoint), &vertex_data(&exp_src)) &&
+                        int_match(contracted_vertex, &vertex_data(&exp_dest)))
+                    {
+                        if (!search_edge(a, &exp_src, &exp_dest)) {
+                            graph_ins_e(a, &exp_src, &exp_dest);
+                        }
+                    }
+                } else {
+                    if (!search_edge(a, &exp_src, &exp_dest)) {
+                        graph_ins_e(a, &exp_src, &exp_dest);
+                    }
+                }
+            }
+            visited[dest] = true;
+            dest = src;
+            src = parents[dest] ? vertex_data(parents[dest]) : 0;
+        }
+        visited[dest] = true;
+    }
+}
+
+static bool search_edge(const Graph* g, const void* v, const void* w)
+{
+    AdjList *adj = NULL;
+
+    for (ListElt* e = list_head(&g->_adjlists); e; e = list_next(e)) {
+        if (g->match(((AdjList*)list_data(e))->_vertex, v) == 0) {
+            adj = (AdjList*)list_data(e);
+            break;
+        }
+    }
+    if (!adj) {
+        return false;
+    }
+    for (ListElt* e = list_head(&adj->_adj); e; e = list_next(e)) {
+        if (g->match(list_data(e), w) == 0) {
+            ((Vertex*)w)->_weight = vertex_weight(list_data(e));
+            return true;
+        }
+    }
+    return false;
 }
 
 static int int_match(const void* i, const void* j)
